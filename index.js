286c8f6af99abbe6d236e433fde851d30601b182
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-02-22 23:50:38 +0530

    22feb26

--- index.js
+++ index.js
@@ -76,3 +76,82 @@
 
 
 // CALLBACK HELL SOLUTION-3(USING PROMISES)
+// AVAILABLE PROMISE STATE AND VALUES
+// PENDING(UNDEFINED)
+// FULLFILLED(RESOLVED VALUE)
+// REJECTED(REASON FOR REJECTION)
+// AVAILABLE PROMIS METHODS
+// Promise.all([promises])
+// Promise.allSettled([promises])
+// Promise.any([promises])
+// Promise.race([promises])
+
+// function step1(value,error){
+//     return new Promise((resolve,reject)=>{
+//         if(!error){
+//             resolve(value+10);
+//         }else{
+//             reject('something went wrong');
+//         }
+//     });
+// }
+// function step2(value,error){
+//     return new Promise((resolve,reject)=>{
+//         if(!error){
+//             resolve(value+10);
+//         }else{
+//             reject('something went wrong');
+//         }
+//     });
+// }
+// function step3(value,error){
+//     return new Promise((resolve,reject)=>{
+//         if(!error){
+//             resolve(value+10);
+//         }else{
+//             reject('something went wrong');
+//         }
+//     });
+// }
+// step1(10,true)
+// .then((result1)=>step2(result1,false))
+// .then((result2)=>step3(result2,false))
+// .then((result3)=>console.log(result3))
+// .catch((error)=>console.log(error));
+
+
+// const p1=Promise.resolve('it is correct');
+// const p2=100;
+// const p3= new Promise((resolve,reject)=>{
+//     setTimeout(resolve,1000,'not correct');
+// });
+// const p4=Promise.reject('rejected');
+// Promise.all([p1,p2,p3]).then((values)=>console.log(values));
+// Promise.race([p2,p3,p1]).then((value)=>console.log(value));
+// Promise.allSettled([p1,p2,p3,p4]).then((value)=>console.log(value));
+// Promise.any([p4,p3]).then((value)=>console.log(value));
+
+// fetch("https://api.github.com/users")
+// .then((response)=>response.json())
+// .then((result)=>console.log(result));
+
+// CHAINING OF PROMISES(async/await)
+function step1(value,error){
+    return new Promise((resolve,reject)=>{
+        if(!error){
+            resolve(value+10)
+        }else{
+            reject('something went wrong');
+        }
+    });
+}
+async function result(){
+    let result1=await step1(10,false);
+    // console.log(result1);
+    return result;
+}
+async function result2(){
+    let result=await Promise.resolve(5);
+    return result;
+}
+console.log(result());
