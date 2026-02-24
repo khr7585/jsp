c02bcb5414ce0fddba6a609531d767afe1816f0e
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-02-24 22:53:33 +0530

    24feb26

---
+++ errorhandling.js
@@ -0,0 +1,15 @@
+// try{
+//     console.log("hello");
+// console.log(welcome);
+// }
+// catch(error){ //console.log(error);//error.name // console.log("error");
+// }
+// console.log("welcome");
+
+// let x=10;
+// console.log("hello");
+// if(x<15){ //x<1
+//     throw "gun with bullets" //throw key word means error occurs
+//     console.log("welcome");
+// }
+// console.log("hello welcome");
--- index.js
+++ index.js
@@ -136,22 +136,22 @@
 // .then((result)=>console.log(result));
 
 // CHAINING OF PROMISES(async/await)
-function step1(value,error){
-    return new Promise((resolve,reject)=>{
-        if(!error){
-            resolve(value+10)
-        }else{
-            reject('something went wrong');
-        }
-    });
-}
-async function result(){
-    let result1=await step1(10,false);
-    // console.log(result1);
-    return result;
-}
-async function result2(){
-    let result=await Promise.resolve(5);
-    return result;
-}
-console.log(result());
+// function step1(value,error){
+//     return new Promise((resolve,reject)=>{
+//         if(!error){
+//             resolve(value+10)
+//         }else{
+//             reject('something went wrong');
+//         }
+//     });
+// }
+// async function result(){
+//     let result1=await step1(10,false);
+//     // console.log(result1);
+//     return result;
+// }
+// async function result2(){
+//     let result=await Promise.resolve(5);
+//     return result;
+// }
+// console.log(result());
