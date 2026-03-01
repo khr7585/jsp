305438d1304eb24fc58de7457c327820aec57751
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-03-01 23:24:31 +0530

    01mar26

--- simpleprojects/example/social/social.js
+++ simpleprojects/example/social/social.js
@@ -1,18 +1,18 @@
-let likebtn=document.querySelectorAll("like");
-let likecount=document.querySelectorAll("btncount");
-likebtn.forEach(likebtn =>{
-let count=0;
-likebtn.addEventListener("click",function(){
-    likebtn.classList.toggle("liked");
-    if(likebtn.classList.contains("liked")){
-        likebtn.classList.remove("bi-heart");
-        likebtn.classList.add("bi-heart-fill");
-        count++;
-    }else{
-        likebtn.classList.remove("bi-heart-fill");
-        likebtn.classList.add("bi-heart");
-        count--;
+const likebtn= document.querySelectorAll(".bi");
+likebtn.forEach(heart => {
+  heart.addEventListener("click", function () {
+    const countSpan = this.nextElementSibling;
+    let count = parseInt(countSpan.textContent);
+    this.classList.toggle("liked");
+    if (this.classList.contains("liked")) {
+      this.classList.remove("bi-heart");
+      this.classList.add("bi-heart-fill");
+      count++;
+    } else {
+      this.classList.remove("bi-heart-fill");
+      this.classList.add("bi-heart");
+      count--;
     }
-    likecount.textContent=count;
-});
+    countSpan.textContent = count;
+  });
 });
