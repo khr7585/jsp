da284ce9850c39a2e53928574fc9e11cca048b19
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-03-09 23:27:31 +0530

    feat: just created navbar for advanced socialmediaproject

---
+++ simpleprojects/socialmedia-advancedproject/interface.js
@@ -0,0 +1,12 @@
+function toggleMenu(){
+
+let menu = document.getElementById("menuBox");
+
+if(menu.style.display === "flex"){
+menu.style.display = "none";
+}else{
+menu.style.display = "flex";
+}
+
+}
+funtion 
